d52710cacc71150738c0d0946eb3a10949cba93f
Enzo DJABALI <enzo.djabali@supinfo.com>
2022-10-27 13:54:11 +0200

    Fix bug redirection après création d'un compte

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -37,7 +37,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_todo_index');
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
